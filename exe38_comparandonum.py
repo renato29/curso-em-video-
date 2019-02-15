41fcb2af9c74a9bb22457878860dd72a5d8c0dbc
Renato <renato.szk@hotmail.com>
2019-02-15 19:34:31 -0200

    Add files via upload

---
+++ exe38_comparandonum.py
@@ -0,0 +1,8 @@
+n1 = int(input('digite n1:'))
+n2 = int(input('digite o n2:'))
+if n1>n2:
+    print('o numero n1 é maior que n2')
+elif n2>n1:
+    print('n2 é maior do que n1')
+elif n1==n2:
+    print('os dois valores sao iguais')
